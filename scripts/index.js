301ede30e8279c53419f2a4be1a91e2edad7e1b1
Pablo Peralta <pablo.cavs.86@gmail.com>
2022-07-20 00:29:48 -0300

    Proyecto javascript para Coderhouse

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,11 @@
-function b64_to_utf8( str ) {
-    return decodeURIComponent(escape(window.atob( str )));
-}
-
 function cargarArchivo(p,p1){
-    document.getElementById(p).src = URL.createObjectURL(document.getElementById(p1).files[0]);
-    document.getElementById("resultado").innerText = "Scanning QR Code...";
-
+    if (document.getElementById(p1).files[0] !== undefined) {
+        document.getElementById(p).src = URL.createObjectURL(document.getElementById(p1).files[0]);
+        document.getElementById("resultado").innerText = "Escaneando codigo QR...";
+    } else {
+        return;
+    }
+    
     let configuraciones = {
         scale: {
             once: true,
@@ -18,50 +18,70 @@ function cargarArchivo(p,p1){
         jsQR: {}
     };
     
-let archivo = document.getElementById(p).src
+    let archivo = document.getElementById(p).src;
+    
+    let callback = function (result) {
+        if (result.success) {
+            console.log(result.codes);
+            document.getElementById("resultado").innerText = result.codes[0].replace("https://www.afip.gob.ar/fe/qr/?p=","");
+            let decodificadoB64 = atob(document.getElementById("resultado").value);
+            console.log(typeof decodificadoB64);
+            document.getElementById("resultadoDecodificado").innerText = decodificadoB64;
+            console.log(decodificadoB64);
+            let datosComprobante = JSON.parse(decodificadoB64);
 
-let callback = function (result) {
-    if (result.success) {
-        console.log(result.codes)
-        document.getElementById("resultado").innerText = result.codes[0].replace("https://www.afip.gob.ar/fe/qr/?p=","");
-        let decodificadoB64 = atob(document.getElementById("resultado").value);
-        console.log(typeof decodificadoB64)
-        document.getElementById("resultadoDecodificado").innerText = decodificadoB64;
-        console.log(decodificadoB64);
-        let datosComprobante = JSON.parse(decodificadoB64);
-        console.log(datosComprobante.cuit);
-        console.log(datosComprobante.nroCmp);
-        console.log(datosComprobante.ptoVta);
-        console.log(datosComprobante.importe);
-    } else {
-        console.log(result.message);
-        document.getElementById("resultado").innerText = result.message;
+            document.getElementById("numeroComprobante").value = datosComprobante.ptoVta + "-" + datosComprobante.nroCmp;
+            validarNroComprobante("numeroComprobante");
+            document.getElementById("fechaComprobante").value = datosComprobante.fecha;
+            
+            let tipoDeComprobante = "000" + datosComprobante.tipoCmp;
+            tipoDeComprobante = tipoDeComprobante.substring(tipoDeComprobante.length-3,tipoDeComprobante.length);
+            document.getElementById("tipoComprobante").value = tipoDeComprobante;
+
+            document.getElementById("cuit").value = datosComprobante.cuit;
+            document.getElementById("nroAutorizacion").value = datosComprobante.codAut;
+            document.getElementById("moneda").value = datosComprobante.moneda.toUpperCase();
+            document.getElementById("cotizacion").value = datosComprobante.ctz;
+            document.getElementById("importe").value = datosComprobante.importe;
+
+            console.log(datosComprobante.fecha);
+            console.log(datosComprobante.cuit);
+            console.log(datosComprobante.ptoVta);
+            console.log(datosComprobante.tipoCmp);
+            console.log(datosComprobante.nroCmp);
+            console.log(datosComprobante.importe);
+            console.log(datosComprobante.moneda);
+            console.log(datosComprobante.ctz);
+            console.log(datosComprobante.nroDocRec);
+            console.log(datosComprobante.codAut);
+        } else {
+            console.log(result.message);
+            document.getElementById("resultado").innerText = result.message;
+        }
     }
-}
-
-PDF_QR_JS.decodeSinglePage(archivo, 1, configuraciones, callback);    
-
-}
-
-function cargarArchivo2(p,p1){
-    document.getElementById(p).src = URL.createObjectURL(document.getElementById(p1).files[0]);
-    let archivo = document.getElementById(p1).files[0]
-    let formData = new FormData();
-    formData.append('file', archivo);
-
-    document.getElementById("resultado").innerText = "Scanning QR Code...";
-    fetch("http://api.qrserver.com/v1/read-qr-code/", {
-        method: 'POST', body: formData
-    }).then(res => res.json()).then(result => {
-        result = result[0].symbol[0].data;
-        document.getElementById("resultado").innerText = result ? "Upload QR Code to Scan" : "Couldn't scan QR Code";
-        if(!result) return;
-        document.querySelector("textarea").innerText = result;
-    }).catch(() => {
-        infoText.innerText = "Couldn't scan QR Code";
-    });
+    
+    PDF_QR_JS.decodeSinglePage(archivo, 1, configuraciones, callback);    
 
 }
+
+// api para leer qr desde una imagen
+// function cargarArchivo2(p,p1){
+//     document.getElementById(p).src = URL.createObjectURL(document.getElementById(p1).files[0]);
+//     let archivo = document.getElementById(p1).files[0]
+//     let formData = new FormData();
+//     formData.append('file', archivo);
+//     document.getElementById("resultado").innerText = "Scanning QR Code...";
+//     fetch("http://api.qrserver.com/v1/read-qr-code/", {
+//         method: 'POST', body: formData
+//     }).then(res => res.json()).then(result => {
+//         result = result[0].symbol[0].data;
+//         document.getElementById("resultado").innerText = result ? "Upload QR Code to Scan" : "Couldn't scan QR Code";
+//         if(!result) return;
+//         document.querySelector("textarea").innerText = result;
+//     }).catch(() => {
+//         infoText.innerText = "Couldn't scan QR Code";
+//     });
+// }
 
 function validarNroComprobante(p){
     let nroComprobante = document.getElementById(p).value;
@@ -125,117 +145,49 @@ function validarCUIT(p){
     }
 }
 
-/*
-new Cliente (nombre, apellido, edad, domicilio, codigoPostal, ciudad, pais, estadoCivil)
-
-class Clientes {
-    constructor(nombre, apellido, edad, domicilio, codigoPostal, ciudad, pais, estadoCivil) {
-    this.nombre = nombre;
-    this.apellido = apellido; 
-    this.edad = edad;
-    this.domicilio = domicilio; 
-    this.codigoPostal = codigoPostal; 
-    this.ciudad = ciudad;
-    this.pais = pais;
-    this.estadoCivil = estadoCivil;
+class Comprobante {
+    constructor(numeroComprobante, fechaComprobante, tipoComprobante, cuit, nroAutorizacion, moneda, cotizacion, importe) {
+    this.numeroComprobante = numeroComprobante;
+    this.fechaComprobante  = fechaComprobante; 
+    this.tipoComprobante   = tipoComprobante;
+    this.cuit              = cuit; 
+    this.nroAutorizacion   = nroAutorizacion; 
+    this.moneda            = moneda;
+    this.cotizacion        = cotizacion;
+    this.importe           = importe;
     }
 }
 
-const cliente1 = new Clientes;
+const comprobantesGuardados = [];
 
-*/
+function guardarComprobante(){
+    // falta funcion valida que esten todos los campos completos y con datos validos
+    // falta funcion para borrar todos los campos despues de guardar
+
+    const numeroComprobante = document.getElementById("numeroComprobante").value;
+    const fechaComprobante = document.getElementById("fechaComprobante").value;
+    const tipoComprobante = document.getElementById("tipoComprobante").value;
+    const cuit = document.getElementById("cuit").value;
+    const nroAutorizacion = document.getElementById("nroAutorizacion").value;
+    const moneda = document.getElementById("moneda").value;
+    const cotizacion = document.getElementById("cotizacion").value;
+    const importe = document.getElementById("importe").value;
+
+    const nuevoComprobante = new Comprobante(numeroComprobante, fechaComprobante, tipoComprobante, cuit, nroAutorizacion, moneda, cotizacion, importe);
+    comprobantesGuardados.push(nuevoComprobante);
+}
 
 
-
-function decodePdf() {
-
-/*    const resultHolder = document.getElementById('holder');
-    const decoder = document.getElementById('decode_btn');
-    var summarydiv = document.getElementById('summarydiv');
-    var summaryBody = document.getElementById('summary').getElementsByTagName('tbody')[0];
-    resultHolder.innerHTML = '';
-    summaryBody.innerHTML = '';
-    summarydiv.style.display = 'none';
-    decoder.disabled = true;
-*/
-    // this function is used to catch the result returned from PDFQrJS
-    function recordcallback(result) {
-        console.log("PDF-QR.js result : ");
-        console.log(result);
-        if (result.success) {
-
-            if (result.codesByPage !== undefined) // full document scanned
-            {
-                for (var page in result.codesByPage) {
-                    var div = document.getElementById('result_page' + page);
-                    if (result.codesByPage[page].length > 0)
-                        div.innerText = "QR Codes found : " + result.codesByPage[page].join(', ');
-                    else
-                        div.innerText = "No QR Codes found!";
-                }
-            } else { // single page scanned
-                var div = document.getElementById('result_page' + result.decodedPage);
-                if (result.codes.length > 0)
-                    div.innerText = "QR Codes found : " + result.codes.join(', ');
-                else
-                    div.innerText = "No QR Codes found!";
-            }
-
-            var keyDescript = {totalOnPatch: "patch", totalOnScale: "scale"};
-            for (var key in result.stats) {
-                for (var k in result.stats[key]) {
-                    var str = result.stats[key][k] + " QR Codes found on " + k + " " + keyDescript[key];
-                    addrow(str);
-                }
-            }
-            summarydiv.style.display = 'block';
-        } else {
-            console.log(result.message);
-            holder.innerText = result.message;
+function verComprobantesGuardados(){
+    console.log("Todos los comprobantes:", comprobantesGuardados);
+    const comprobantesGuardadosDolares = comprobantesGuardados.filter(actual => actual.moneda === "DOL");
+    console.log("Comprobantes en Dolares:", comprobantesGuardadosDolares);
+    const comprobantesGuardadosPesos = comprobantesGuardados.filter(actual => actual.moneda === "PES");
+    console.log("Comprobantes en Pesos:", comprobantesGuardadosPesos);
+    console.log("Total de Comprobantes guardados:", comprobantesGuardados.length);
+    comprobantesGuardados.forEach((actual, i) => {
+        if (actual.importe > 10000) {
+            console.log(`El comprobante: ${actual.numeroComprobante} es mayor a 10,000.00`)
         }
-
-        decoder.disabled = false;
-    }
-
-    // function which prints pages on the demo (it is an optional parameter)
-    function printTestImg(pageAsImg, pageNr) {
-
-        var div = document.createElement('div');
-        div.innerText = "Page_" + pageNr;
-        div.classList = 'resultContainer';
-        resultHolder.appendChild(div);
-
-        pageAsImg.classList = 'resultPage';
-        resultHolder.appendChild(pageAsImg);
-
-        var resultsDiv = document.createElement('div');
-        resultsDiv.id = "result_page" + pageNr;
-        resultsDiv.classList = 'resultDiv';
-        resultsDiv.innerHTML = "<img src='./media/loading.gif' height='70px' width='70px;'>";
-        resultHolder.appendChild(resultsDiv);
-    }
-
-    // decode pdf call (all code you need to call in order to scan qr codes in you pdfs
-    var input_file = document.getElementById('pdfentryfile');
-    if (input_file.value === "") {
-        alert('Please provide a valid pdf file!');
-        decoder.disabled = false;
-        return false;
-    }
-
-    if (decodeType === "allpages") {
-        PDF_QR_JS.decodeDocument(input_file, configs,
-            recordcallback, printTestImg
-        );
-    } else {
-        var pageNrInput = document.getElementById('pageNr');
-        if (pageNrInput.value === '' || parseInt(pageNrInput.value) <= 0) {
-            alert('Please provide a valid page number!');
-            decoder.disabled = false;
-            return false;
-        }
-        PDF_QR_JS.decodeSinglePage(input_file, parseInt(pageNrInput.value), configs,
-            recordcallback, printTestImg
-        );
-    }
+    })
 }
